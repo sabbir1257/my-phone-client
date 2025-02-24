dbf7f71417e12596690baba56ab67827303d353c
sabbir1257 <masabbir1257@gmail.com>
2025-02-24 10:06:38 +0600

    Load single data using get api

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+             
+            <Link to="/phones">Phones</Link>
+        </div>
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Main.jsx";
+import Phones from "./components/Phones.jsx";
+import Phone from "./components/Phone.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/phones",
+        element: <Phones />,
+        loader: () => fetch('http://localhost:5000/phones')
+      },
+      {
+        path: '/phone/:id',
+        element: <Phone/>,
+        loader:({params}) => fetch(`http://localhost:5000/phones/${params.id}`)
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
